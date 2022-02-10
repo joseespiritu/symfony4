162022491a2e20f12b1f3f40cc9267b436b26c89
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-02-09 21:31:52 -0600

    primeros pasos, rutas, parametros

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'hello' => 'Hola Mundo con Symfony 4',
+        ]);
+    }
+
+    public function animales($nombre, $apellidos): Response {
+        $title = 'Bienvenido a la pagina de Animales';
+        return $this->render('home/animales.html.twig', [
+            'title' => $title,
+            'nombre' => $nombre,
+            'apellidos' => $apellidos,
+        ]);
+    }
+
+}
